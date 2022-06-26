477d9608b7eb8048313b589a94cf18355ef74651
satishkumarojha <satishkumarojha15@gmail.com>
2022-06-27 02:24:43 +0530

    completed with localstorage

---
+++ React_redux_2/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Home } from './pages/Home'
+import {Routes,Route} from 'react-router-dom';
+import { Login } from './pages/Login';
+import { Editpage } from './pages/Editpage';
+import { Privateauth } from './hoc/Privateauth';
+function App() {
+
+
+  return (
+    <div className="App">
+     
+      <Routes>
+        <Route path='/' element={<Privateauth> <Home/></Privateauth>}/>
+        <Route path='/login' element={ <Login/>}/>
+        <Route path='/todos/:id' element={ <Editpage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
