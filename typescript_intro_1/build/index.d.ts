b11fd6e3c10141fefbd18ed66750e11c4bd7193c
satishkumarojha <satishkumarojha15@gmail.com>
2022-07-14 00:01:27 +0530

    typescript 1 completed

--- typescript_intro_1/build/index.d.ts
+++ typescript_intro_1/build/index.d.ts
@@ -4,3 +4,12 @@ declare let isFetching: boolean;
 declare let arr: number[];
 declare let str: string[];
 declare let tupple: [string, boolean];
+declare enum users {
+    User = 0,
+    SuperUser = 1,
+    Admin = 2,
+    SuperAdmin = 3
+}
+declare function product(x: number, y: number): number;
+declare function devide(x: number, y: number): number;
+declare function printname(name: string): void;
